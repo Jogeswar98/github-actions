20628aa0d50dd33b1b75bf178c359624f875f36c
Jogeswar98 <jogeswarpradhan98@gmail.com>
2025-10-22 16:26:18 +0530

    Add AWS provider and EC2 instance configuration

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-08e5424edfe926b43"  # Ubuntu AMI (Mumbai region)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GitHubActionDemo"
+  }
+}
