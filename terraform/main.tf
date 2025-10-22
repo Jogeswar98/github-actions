0a29f430b72f94aafb49b6dba67868870b970d43
Jogeswar98 <jogeswarpradhan98@gmail.com>
2025-10-22 17:45:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
+region = "ap-south-1"
 }
- region = "ap-south-1"
-
+ 
 resource "aws_instance" "example" {
   ami           = "ami-08e5424edfe926b43"  # Ubuntu AMI (Mumbai region)
   instance_type = "t2.micro"
